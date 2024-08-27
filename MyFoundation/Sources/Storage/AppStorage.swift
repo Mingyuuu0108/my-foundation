0e34a27ad2ce877084911878a47cdeaf748c8bb7
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2024-08-27 17:09:53 +0900

    add: Package.swift

---
+++ MyFoundation/Sources/Storage/AppStorage.swift
@@ -0,0 +1,39 @@
+//
+//  AppStorage.swift
+//  MyFoundation
+//
+//  Created by 이민규 on 8/27/24.
+//
+
+import Foundation
+
+open class AppStorage {
+    public static let standard = UserDefaults.standard
+    
+    public static func get(for key: String) -> String {
+        guard let value = standard.string(forKey: key) else {
+            return "error"
+        }
+        return value
+    }
+    
+    public static func set(_ value: String, for key: String) {
+        standard.setValue(value, forKey: key)
+    }
+    
+    public static func remove(for key: String) {
+        standard.removeObject(forKey: key)
+    }
+    
+    public static func read(for key: String) {
+        if let value = standard.string(forKey: key) {
+            print("\(key) : \(value)")
+        }
+    }
+    
+    public static func readAll() {
+        for (key, value) in standard.dictionaryRepresentation() {
+            print("\(key) : \(value)")
+        }
+    }
+}
